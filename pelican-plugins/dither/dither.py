b987ce62229cf5f220ca14a0d94618cf36aba35f
rra <r.roscamabbing@artez.nl>
2018-06-21 21:57:46 +0200

    now supports lists of to-be transparent pixels and counts savings in KiB

--- pelican-plugins/dither/dither.py
+++ pelican-plugins/dither/dither.py
@@ -35,24 +35,34 @@ DEFAULT_IMAGE_DIR = "images"
 DEFAULT_DITHER_DIR = "dithers"
 DEFAULT_THRESHOLD = [96, 96, 96] # this sets the contrast of the final image, rgb
 DEFAULT_TRANSPARENCY= True
-DEFAULT_TRANSPARENT_COLOR = (125,125,125)
-DEFAULT_DITHER_PALETTE = [(25,25,25), (75,75,75),(125,125,125),(175,175,175),(225,225,225), (250,250,250)] # 6 tone palette
+DEFAULT_TRANSPARENT_COLOR = [(125,125,125)]
+DEFAULT_DITHER_PALETTE = [(25,25,25), (75,75,75),(125,125,125),(175,175,175),(225,225,225),(250,250,250)] # 6 tone palette
 
 #11 tone palette, heavier, more detail, less visible dither pattern
-#[(0,0,0), (25,25,25), (50,50,50),(75,75,75),(100,100,100),(125,125,125),(150,150,150),(175,175,175),(200,200,200), (225,225,225), (250,250,250)]
+#[(0,0,0),(25,25,25),(50,50,50),(75,75,75),(100,100,100),(125,125,125),(150,150,150),(175,175,175),(200,200,200),(225,225,225),(250,250,250)]
 
 #3 tone palette, lighter, heavier dithering effect
-# [(0,0,25), (125,125,225), (250,250,250)]
+# [(0,0,0), (125,125,125), (250,250,250)]
 
 
 def _image_path(pelican):
     return os.path.join(pelican.settings['PATH'],
         pelican.settings.get("IMAGE_PATH", DEFAULT_IMAGE_DIR)).rstrip('/')
 
-
 def _out_path(pelican):
     return os.path.join(pelican.settings['OUTPUT_PATH'],
                          pelican.settings.get('DITHER_DIR', DEFAULT_DITHER_DIR))
+
+def calculate_savings(image_dir, dither_dir):
+    original = 0
+    dithered = 0
+    for i in os.listdir(image_dir):
+        original = original + os.stat(os.path.join(image_dir, i)).st_size
+
+    for i in os.listdir(dither_dir):
+        dithered = dithered + os.stat(os.path.join(dither_dir, i)).st_size
+
+    return 'dither plugin:\ntotal size image directory  :{}KiB\ntotal size dithered directory:{}KiB'.format(original/1000, dithered/1000)
 
 def color_to_transparent(img, color):
     #make all pixels of a certain color transparent
@@ -75,7 +85,6 @@ def dither(pelican):
     in_path = _image_path(pelican)
     out_path = _out_path(pelican)
 
-
     transparency = pelican.settings.get("TRANSPARENCY",DEFAULT_TRANSPARENCY)
 
     if not os.path.exists(out_path):
@@ -88,23 +97,26 @@ def dither(pelican):
             of = os.path.join(out_path, filename.replace(ext,'.png'))
             if not os.path.exists(of):
                 logging.debug("dither plugin: dithering {}".format(fn))
+
                 img= Image.open(fn).convert('RGB')
-
+                
                 palette = hitherdither.palette.Palette(pelican.settings.get('DITHER_PALETTE', DEFAULT_DITHER_PALETTE))
-
+                
                 threshold = pelican.settings.get('THRESHOLD', DEFAULT_THRESHOLD)
-
+                
                 img_dithered = hitherdither.ordered.bayer.bayer_dithering(img, palette, threshold, order=8) #see hither dither documentation for different dithering algos
 
                 if transparency:
                     #pick a color to become transparent, make sure you pick a tint in the middle of the palette for optimum effect
                     # you can also rerun the function to make more colros transparent
                     transparent_color= pelican.settings.get('TRANSPARENT_COLOR',DEFAULT_TRANSPARENT_COLOR)
-                    logging.debug("dither plugin: Replaced color {},{},{} by transparency on {}".format(transparent_color[0],transparent_color[1],transparent_color[2], fn))
-                    transparent_image = color_to_transparent(img_dithered, transparent_color)
-                    img_dithered = transparent_image
+                    for colorset in transparent_color:
+                        logging.debug("dither plugin: Replaced color {},{},{} by transparency on {}".format(colorset[0],colorset[1],colorset[2], fn))
+                        transparent_image = color_to_transparent(img_dithered, colorset)
+                        img_dithered = transparent_image
          
                 img_dithered.save(of)
+    logging.debug(calculate_savings(in_path,out_path))
 
 def parse_for_images(instance):
     #based on better_figures_and_images plugin by @dflock, @phrawzty,@if1live,@jar1karp,@dhalperi,@aqw,@adworacz
